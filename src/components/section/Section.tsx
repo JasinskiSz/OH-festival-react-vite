b02d54b1ed20406f4c426a9f27131c91d503b4f0
Szymon Jasiński <szg.jasinski@gmail.com>
2024-04-21 17:40:13 +0200

    feat: add navbar structure
    Create navbar with first few components and coresponding css files.

---
+++ src/components/section/Section.tsx
@@ -0,0 +1,10 @@
+import PageNav from '../page-nav/PageNav';
+const Section: React.FC = () => {
+    return (
+        <>
+            <PageNav></PageNav>
+        </>
+    )
+}
+
+export default Section;
