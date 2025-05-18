9d2766f9abc204187748a274e7b3b16116ff5f20
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-05-18 21:47:41 +0200

    feat: add four circles in hero section
    Add two small circles in the center, visible in the middle position
    of the screen.
    Add two bigger, dark circles on the edges of the viewport.
    Add CSS properties handling placement and appearance of the elements.
    
    Signed-off-by: Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,34 @@
-import './App.css'
+import './App.css';
+import "../FlexClasses.css";
 import PageNav from "../layout/page-nav/PageNav";
+import HeroButton from '../components/hero-button/HeroButton';
 
 function App() {
+  const imagePath = ["/banana.png", "/abstract.png"];
 
   return (
-    <header>
-      <PageNav/>
-    </header>
+    <>
+      <header className="flex-column flex-center-cross">
+        <PageNav />
+      </header>
+      <main className="flex-center-main column flex-grow-1">
+        <div className="flex-center-cross">
+          <div id="left-black-disk" className="flex-column flex-center-main flex-grow-1 black-disk">
+            <img src={imagePath[0]} alt="banana" id="banana" />
+          </div>
+          <div id="gdynia-disk" className="flex-column flex-center flex-grow-1">
+            <HeroButton text="Gdynia" direction="right" colorHash="#1F0812" />
+          </div>
+          <div id="gdansk-disk" className="flex-column flex-center flex-grow-1">
+            <HeroButton text="Gdańsk" direction="left" colorHash="#FBFDFF" />
+          </div>
+          <div id="right-black-disk" className="flex-column flex-center-main flex-grow-1 black-disk">
+            <img src={imagePath[1]} alt="abstract-image" id="abstract" />
+          </div>
+        </div>
+      </main>
+
+    </>
   )
 }
 
---
+++ src/components/hero-button/HeroButton.tsx
@@ -0,0 +1,39 @@
+import "./HeroButton.css";
+import "../../FlexClasses.css";
+
+interface Props {
+    text: string;
+    direction: "left" | "right";
+    colorHash: string;
+}
+
+const handleDirection = (direction: string, text: string) => {
+    switch (direction) {
+        case "right":
+            return (
+                <>
+                    {text}
+                    <img src="/Icons/Arrow-right.svg" alt="arrow-right" />
+                </>
+            );
+        case "left":
+            return (
+                <>
+                    <img src="/Icons/Arrow-left.svg" alt="arrow-left" id="right-circle-text" />
+                    {text}
+                </>
+            );
+        default:
+            return "";
+    }
+}
+
+const HeroButton = ({ text, direction, colorHash }: Props) => {
+    return (
+        <a href="#" id="hero-button" className="flex-center-cross" style={{ color: colorHash }}>
+            {handleDirection(direction, text)}
+        </a>
+    );
+}
+
+export default HeroButton;
